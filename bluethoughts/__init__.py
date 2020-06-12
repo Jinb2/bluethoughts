c26b6754dfaf6712a38c1c8f7b6e08c3b1fdf761
Benson <36316652+Jinb2@users.noreply.github.com>
2020-06-12 19:55:20 -0400

    Update __init__.py

--- bluethoughts/__init__.py
+++ bluethoughts/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f8bcff1dc840085af10535ca4673c3a5'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 
