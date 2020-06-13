d45dac93e7ac8dc20986d27f710a060e686ef5cc
Benson <36316652+Jinb2@users.noreply.github.com>
2020-06-12 23:30:45 -0400

    changed URI

--- bluethoughts/__init__.py
+++ bluethoughts/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f8bcff1dc840085af10535ca4673c3a5'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://evznnauhsmzzgz:b41bc5a04b725c8bc57436cc50531b0c045bfc3682152a98a81108f9b8d78a29@ec2-34-194-198-176.compute-1.amazonaws.com:5432/dfmaaj5vr1v8it"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ythqzyzjmjsbmv:b6f387ce17de6f3482de3b4e6459c70341e3aba3912a5f15eb0ea268b07c0ce4@ec2-52-44-55-63.compute-1.amazonaws.com:5432/deu74ncivoamea"
 db = SQLAlchemy(app)
 
 
