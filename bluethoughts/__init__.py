ca9c0d8e3cea4d37215a31eae023236ea9a86d2b
Benson <36316652+Jinb2@users.noreply.github.com>
2020-06-14 16:35:53 -0400

    Update __init__.py

--- bluethoughts/__init__.py
+++ bluethoughts/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f8bcff1dc840085af10535ca4673c3a5'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ythqzyzjmjsbmv:b6f387ce17de6f3482de3b4e6459c70341e3aba3912a5f15eb0ea268b07c0ce4@ec2-52-44-55-63.compute-1.amazonaws.com:5432/deu74ncivoamea"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://jwosqyfhfemshm:ba16df90a62f15906e24efcd85eeb2998159a78820485d98482ec880eed2b135@ec2-18-213-176-229.compute-1.amazonaws.com:5432/d1uv744s883dai"
 db = SQLAlchemy(app)
 
 
